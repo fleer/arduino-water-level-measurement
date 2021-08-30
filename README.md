d578eda38e018e0cf44284e06141667989cffcef
fleer <sascha.fleer@uni-bielefeld.de>
2021-08-30 22:34:32 +0200

    Modified code to work with .html files
    - The code now relies on a .html file that is stored on the arduino
    - The rainwater cistern has a volume of 5000l and a depth of 1.785m

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # arduino-water-level-measurement
+
+The whole project was inspired by the work described in the follwoing posts:
+
+- (https://arduino-projekte.info/zisterne-fuellstandsanzeige)[https://arduino-projekte.info/zisterne-fuellstandsanzeige]
+- (https://arduino-projekte.info/zisterne-3-0-web-server-spiffs)[https://arduino-projekte.info/zisterne-3-0-web-server-spiffs]
+
+## Requirements
+
+The following libraries are required:
+
+- (ESPAsyncWebServer)[https://github.com/me-no-dev/ESPAsyncWebServer]
+- (ESPAsyncTCP)[https://github.com/me-no-dev/ESPAsyncTCP]
+
+Just clone them into the `Arduino/libraries` folder.  
+Also add the `ArduinoJson` Library to the project.
+
+## Upload Files
+
+ -  Navigate to the `Sketch` folder an create a now folder called `data`. In this folder, all website-related stuff like images, `.css` or `.html` files are stored.
+ - The flash size of the corresponding _ESP8266 Board_ should be set to `4M (FS:1MB)` or `4M (1M SPIFFS)`.
+- `.html`, `.css` files and icons are uploaded using the (Arduino ESP8266 filesystem uploader)[https://github.com/esp8266/arduino-esp8266fs-plugin].
+- Download a release and put the resulting folder in the `tools` folder of your _Sketchbook_ folder.
+
--- arduino-water-level-measurement.ino
+++ arduino-water-level-measurement.ino
@@ -36,7 +36,6 @@ long liter = 0;
 char* picture = "";
 
 AsyncWebServer server(80);
-//WiFiServer server(80);
 
 /*
  * Some functions:
@@ -44,84 +43,83 @@ AsyncWebServer server(80);
  * getPicture() -> Get corresponding picture
  */
 
-//Berechnung der Liter für die Webseite
+//Compute volume displayed on website
 String getLiter() {
+
   digitalWrite(trigger, LOW);
-  delay(5);
+  delayMicroseconds(2);
   digitalWrite(trigger, HIGH);
-  delay(10);
+  delayMicroseconds(10);
   digitalWrite(trigger, LOW);
   duration = pulseIn(echo, HIGH);
-  // Prüfen ob Messwert zwischen 200cm und 0cm liegt
-  distance = (duration / 2) / 29.1;
-    if (distance >= 200 || distance <= 0)
+
+  distance = (duration / 2.) / 29.1;
+
+  // Check if measurement is in eligible region
+  if (distance >= 178 || distance <= 0)
   {
     distance = 0;
   }
-  else
-  {
-    liter = 199 - distance;
-    liter = liter * 84.7457;
-  }
+
+  liter = 178.5 - distance;
+  liter = liter * 24.57;
   Serial.println(String(liter));
   return String(liter);
 }
 
 // Choose which image to show
 String getImage() {
-  if (liter >= 0 && liter <= 1600)
+  if (liter >= 0 && liter < 500)
   {
     // 0%
     picture = " <path fill='currentColor' d='M16,20H8V6H16M16.67,4H15V2H9V4H7.33A1.33,1.33 0 0,0 6,5.33V20.67C6,21.4 6.6,22 7.33,22H16.67A1.33,1.33 0 0,0 18,20.67V5.33C18,4.6 17.4,4 16.67,4Z' />";
   }
-  else if (liter >= 1600 && liter <= 3200)
+  else if (liter >=500  && liter < 1000)
   {
     // 10%
     picture = " <path fill='currentColor' d='M16,18H8V6H16M16.67,4H15V2H9V4H7.33A1.33,1.33 0 0,0 6,5.33V20.67C6,21.4 6.6,22 7.33,22H16.67A1.33,1.33 0 0,0 18,20.67V5.33C18,4.6 17.4,4 16.67,4Z' />";
   }
-  else if (liter >= 3200 && liter <= 4800)
+  else if (liter >= 1000 && liter < 1500)
   {
     // 20%
     picture = "<path fill='currentColor' d='M16,17H8V6H16M16.67,4H15V2H9V4H7.33A1.33,1.33 0 0,0 6,5.33V20.67C6,21.4 6.6,22 7.33,22H16.67A1.33,1.33 0 0,0 18,20.67V5.33C18,4.6 17.4,4 16.67,4Z' />";
   }
-  else if (liter >= 4800 && liter <= 6400)
+  else if (liter >= 1500 && liter < 2000)
   {
     // 30%
     picture = "   <path fill='currentColor' d='M16,15H8V6H16M16.67,4H15V2H9V4H7.33A1.33,1.33 0 0,0 6,5.33V20.67C6,21.4 6.6,22 7.33,22H16.67A1.33,1.33 0 0,0 18,20.67V5.33C18,4.6 17.4,4 16.67,4Z' />";
   }
-  else if (liter >= 6400 && liter <= 8000)
+  else if (liter >= 2000 && liter < 2500)
   {
     // 40%
     picture = "<path fill='currentColor' d='M16,14H8V6H16M16.67,4H15V2H9V4H7.33A1.33,1.33 0 0,0 6,5.33V20.67C6,21.4 6.6,22 7.33,22H16.67A1.33,1.33 0 0,0 18,20.67V5.33C18,4.6 17.4,4 16.67,4Z' />";
   }
-  else if (liter >= 8000 && liter <= 9600)
+  else if (liter >= 2500 && liter < 3000)
   {
     // 50%
     picture = "<path fill='currentColor' d='M16,13H8V6H16M16.67,4H15V2H9V4H7.33A1.33,1.33 0 0,0 6,5.33V20.67C6,21.4 6.6,22 7.33,22H16.67A1.33,1.33 0 0,0 18,20.67V5.33C18,4.6 17.4,4 16.67,4Z' />";
   }
-  else if (liter >= 9600 && liter <= 12000)
+  else if (liter >= 3000 && liter < 3500)
   {
     // 60%
     picture = "<path fill='currentColor' d='M16,12H8V6H16M16.67,4H15V2H9V4H7.33A1.33,1.33 0 0,0 6,5.33V20.67C6,21.4 6.6,22 7.33,22H16.67A1.33,1.33 0 0,0 18,20.67V5.33C18,4.6 17.4,4 16.67,4Z' />";
   }
-  else if (liter >= 112000 && liter <= 128000)
+  else if (liter >= 3500 && liter < 4000)
   {
     // 70%
     picture = "<path fill='currentColor' d='M16,10H8V6H16M16.67,4H15V2H9V4H7.33A1.33,1.33 0 0,0 6,5.33V20.67C6,21.4 6.6,22 7.33,22H16.67A1.33,1.33 0 0,0 18,20.67V5.33C18,4.6 17.4,4 16.67,4Z' />";
   }
-  else if (liter >= 12800 && liter <= 144000)
+  else if (liter >= 4000 && liter < 4500)
   {
     // 80%
     picture = "<path fill='currentColor' d='M16,9H8V6H16M16.67,4H15V2H9V4H7.33A1.33,1.33 0 0,0 6,5.33V20.67C6,21.4 6.6,22 7.33,22H16.67A1.33,1.33 0 0,0 18,20.67V5.33C18,4.6 17.4,4 16.67,4Z' />";
   }
-  /*
-  else if (liter >= 14400 && liter <= 16000)
+  else if (liter >= 4500 && liter <= 4800)
   {
     // 90%
     picture = "<path fill='currentColor' d='M16,8H8V6H16M16.67,4H15V2H9V4H7.33A1.33,1.33 0 0,0 6,5.33V20.67C6,21.4 6.6,22 7.33,22H16.67A1.33,1.33 0 0,0 18,20.67V5.33C18,4.6 17.4,4 16.67,4Z' />";
   }
-  */
-  else if (liter >= 14400 && liter <= 16000)
+  else // Maximum volume is 5000 liter
   {
     // 100%
     picture = "<path fill='currentColor' d='M16.67,4H15V2H9V4H7.33A1.33,1.33 0 0,0 6,5.33V20.67C6,21.4 6.6,22 7.33,22H16.67A1.33,1.33 0 0,0 18,20.67V5.33C18,4.6 17.4,4 16.67,4Z' />";
@@ -133,47 +131,14 @@ String getImage() {
 // Placeholder in HTML Code
 String colldata(const String& var) {
   Serial.println(var);
-  // TODO: Fix bug in String cast!
-  /*
   if (var == "LITER") {
     return getLiter();
   }
-  */
   if (var == "BAT") {
     return getImage();
   }
   return "";
 }
-
-// Printing whole webpage
-// For debugging
-/*
-void webPageprint() {
-  WiFiClient client = server.available();
-  client.println("<!DOCTYPE html>");
-  client.println("<head>\n<meta charset='UTF-8'>");
-    // Compatibility for mobile devices
-
-    client.println("<meta name='viewport' content='width=device-width, initial-scale=1' />");
-    client.println("<title>Water Level</title>");
-    client.println("<style>");
-      client.println(" .container { display: flex; align-items: center; justify-content: center } img { max-width: 100% } .image { flex-basis: 40% } .text { font-size: 20px; padding-left: 20px; } ");
-    client.println("</style>");
-  client.println("</head>\n<body>");
-  client.println("<H2>Water Level</H2>");
-  client.println("<div class='container'>");
-    client.println("<div class='image'>");
-      client.println("<svg style='width:20%;height:20%' viewBox='0 0 24 24'> <path fill='currentColor' d='M16 20H8V6H16M16.67 4H15V2H9V4H7.33C6.6 4 6 4.6 6 5.33V20.67C6 21.4 6.6 22 7.33 22H16.67C17.41 22 18 21.41 18 20.67V5.33C18 4.6 17.4 4 16.67 4M15 16H9V19H15V16M15 7H9V10H15V7M15 11.5H9V14.5H15V11.5Z' /> </svg>");
-    client.println("</div>");
-    client.println("<div class='text'>");
-      client.println("<h1>");
-        client.print(duration);
-      client.println("</h1>");
-    client.println("</div>");
-  client.println("</div>");
-  client.print("</body>\n</html>");
-}
-*/
 
 void setup() {
   Serial.begin(115200);
@@ -212,11 +177,11 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
+
   Serial.println("");
   Serial.println("WiFi connected");
 
   // Start the server
-  //server.begin();
   Serial.println("Server started");
 
   // Print the IP address
@@ -229,28 +194,29 @@ void setup() {
   Serial.println(WiFi.subnetMask());
   Serial.println("=============================");
 
-  // Webseite Laden
+  // Load Website
   server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(SPIFFS, "/index.html", String(), false, colldata);
   });
 
-  // CSS Datei Laden
+  // Load CSS Style
   server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(SPIFFS, "/style.css", "text/css");
   });
-  // Liter an Webseite übergeben
+
+  // Send volume to website
   server.on("/LITER", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send_P(200, "text/plain", getLiter().c_str());
   });
 
-  // Bild an Webseite übergeben
+  // Send SVG code of image to website
   server.on("/BAT", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send(200, "text/plain", getImage().c_str());
+    request->send_P(200, "text/plain", getImage().c_str());
   });
+
   // Start server
   server.begin();
 }
-
 
 void loop() {
 /*
@@ -266,17 +232,13 @@ void loop() {
   Serial.println(duration);
   Serial.println("=============================");
 
-  String cmd;
-  cmd += "\"";
-
-
-  // webPagePrint();
-
   delay(500);
 */
   // Debugger
+  /*
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
+  */
 }
